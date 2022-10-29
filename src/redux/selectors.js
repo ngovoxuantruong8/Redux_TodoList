54991ddb9f4bb9d0252bef2b0a93409cf30a8839
Trường Ngô <ngovoxuantruong8@gmail.com>
2022-10-29 21:52:08 +0700

    Commit

---
+++ src/redux/selectors.js
@@ -0,0 +1,10 @@
+import { createSelector } from 'reselect';
+
+export const searchTextSelector = (state) => state.filters.search;
+export const todoListSelector = (state) => state.todoList;
+
+export const todoRemainingSelector = createSelector(todoListSelector, searchTextSelector, (todoList, searchText) => {
+    return todoList.filter((todo) => {
+        return todo.name.includes(searchText);
+    });
+});
